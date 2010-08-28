d41152826b19dd7e9ab0bd5ea4a64e9b6f3da328
Kristian Mandrup <kmandrup@gmail.com>
2010-08-28 23:12:42 +0200

    moved stuff around

--- lib/easy-table/rails_config.rb
+++
@@ -1,7 +0,0 @@
-require 'rails3_plugin_toolbox'
-
-Rails3::PluginExtender.new do
-  extend_rails :view do
-    with EasyTable::ViewExt
-  end
-end  
--- spec/easy-table/table/table_spec.rb
+++ spec/easy-table/table/table_spec.rb
@@ -47,5 +47,57 @@ describe EasyTable::ViewExt::Table::Base do
       end
     end
 
+
+    describe '#render_table' do
+      it 'should display a table' do
+        with_engine(:erb) do |e|        
+          res = e.run_template_locals :posts => @posts do %{
+            <%= render_table do %>
+              <%= render_tbody posts do |post| %>
+                <%= data_row post, %w{id title} %>
+              <% end %>  
+            <% end %>
+          }
+          end      
+          res.should match /<table>/
+          res.should match /<tbody>/
+          res.should match /<tr>/
+          res.should match /<td>1<\/td>/
+          res.should match /<td>my post<\/td>/
+        end
+      end
+
+      it 'should display a blank table' do
+        with_engine(:erb) do |e|        
+          res = e.run_template_locals :posts => @posts do %{
+            <%= render_table do %>
+              hello
+            <% end %>
+          }
+          end      
+          res.should match /<table>/
+          res.should match /hello/
+          puts res        
+        end
+      end
+
+      it 'should display a table with attributes' do
+        with_engine(:erb) do |e|        
+          res = e.run_template_locals :posts => @posts do %{
+            <%= render_table :class => 'tables#posts', :id => 'posts_table' do %>
+              <%= render_tbody posts do |post| %>
+                <%= data_row post, %w{id title} %>
+              <% end %>  
+            <% end %>
+          }
+          end      
+          res.should match /<table class="tables#posts" id="posts_table">/
+          res.should match /<tbody>/
+          res.should match /<tr>/
+          res.should match /<td>1<\/td>/
+          res.should match /<td>my post<\/td>/
+          # puts res
+        end
+      end
   end                             
 end
